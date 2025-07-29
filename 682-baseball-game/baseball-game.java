2ad63c461a234dcb38197616ddf58be5d139e5d2
Rhythm Choudhary <160386403+rhythmchoudhary@users.noreply.github.com>
2025-07-30 01:15:14 +0530

    Time: 3 ms (48.12%) | Memory: 42.5 MB (8.62%) - LeetSync

---
+++ 682-baseball-game/baseball-game.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int calPoints(String[] operations) {
+        Stack <Integer> stack = new Stack<>();
+        for(String s: operations)
+        {
+            if(s.equals("C"))
+            {
+                stack.pop();
+
+            }
+            else if(s.equals("D"))
+            {
+                stack.push(2* stack.peek());
+
+            }
+            else if(s.equals("+"))
+            {
+                if(stack.size()>= 2)
+                {
+                    int res1 = stack.pop();
+                    int res2 = stack.pop();
+                    int res = res1+res2;
+                    stack.push(res2);
+                    stack.push(res1);
+                    stack.push(res);
+                }
+            }
+            else
+            {
+                stack.push(Integer.parseInt(s));
+            }
+
+        }
+        int ans =0;
+        while(!stack.isEmpty())
+        {
+            ans += stack.pop();
+        }
+        return ans;
+    }
+}
